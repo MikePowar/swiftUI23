e3114ee2f626a52c79fb47d3c43a5c009d58fbf4
MikePowar <mike.powar@gmail.com>
2023-10-11 15:27:14 -0700

    Alert Messages

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,23 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack{
-//            Button("Button 1") {}
-//                .buttonStyle(.bordered)
-//            Button("Button 2", role: .destructive) {}
-//                .buttonStyle(.bordered)
-//            Button("Button 3") {}
-//                .buttonStyle(.borderedProminent)
-//                .tint(.mint)
-//            Button("Button 4", role: .destructive) {}
-//                .buttonStyle(.borderedProminent)
-            // CUSTOM BUTTONS:
-            Button {
-                print("Button was tapped")
-            } label: {
-                Label("Edit", systemImage: "car")
-            }
+        Button("Show Alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) { }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Please read this")
         }
     }
 }
